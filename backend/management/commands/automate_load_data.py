0456394af1f4ba29950abd3781c6a956b8529983
Beck <beckyenwen@gmail.com>
2025-07-31 15:51:24 -0600

    fix atomate_commands

--- backend/management/commands/automate_load_data.py
+++ backend/management/commands/automate_load_data.py
@@ -1,21 +1,28 @@
 import os
 import subprocess
+from django.core.management.base import BaseCommand
+
 
 # 檔案名稱列表
 command_files = [
     "add_income_to_community.py",
-    # "load_2016_Census_of_Canada_-_Household_Income_20240806.py",
-    # "load_2023_community_crimes_csv.py",
-    # "load_community_csv.py",
-    # "load_community_services.py"
+    "load_2016_Census_of_Canada_-_Household_Income_20240806.py",
+    "load_2023_community_crimes_csv.py",
+    "load_community_csv.py",
+    "load_community_services.py",
 ]
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-commands_path = os.path.join(base_dir, 'commands')
 
-for command_file in command_files:
-    command_path = os.path.join(commands_path, command_file)
-    try:
-        subprocess.run(["python", command_path], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while running {command_file}: {e}")
+class Command(BaseCommand):
+    help = "Load community incomes from a CSV file into the database"
+
+    def handle(self, *args, **kwargs):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        commands_path = os.path.join(base_dir, "commands")
+
+        for command_file in command_files:
+            command_path = os.path.join(commands_path, command_file)
+            try:
+                subprocess.run(["python", command_path], check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Error occurred while running {command_file}: {e}")
